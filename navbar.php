b92fe42c465a7403722d98bb82f2c59730afb54e
Tom LaPomme <tissmey76@gmail.com>
2018-03-31 01:11:18 +0200

    I fixed some bugs

--- navbar.php
+++ navbar.php
@@ -3,11 +3,12 @@ echo '<div class="logo_home">';
 echo '	<div class="logo_home">
 	   <a id="header" href="index.php.html"><img src="img/logo.png" id="slash" alt="Acceuil"/></a>
     </div>';
+echo '</div>';
 echo '<div class="navbar">';
 echo "<ul>";
 echo '<li><a class="active" href="index.html.php">Acceuil</a></li>
       <li><a href="artiste.html.php">Artiste</a></li>
-      <li><a href="album.html.php">Album</a></li>"';
+      <li><a href="album.html.php">Album</a></li>';
 echo "</ul>";
 echo "</div>";
 ?>

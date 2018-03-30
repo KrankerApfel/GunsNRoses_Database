94104cb3e4ff812b985d5f5afb5091cc546e9aba
Tom LaPomme <tissmey76@gmail.com>
2018-03-30 13:08:48 +0200

    Initial commit

---
+++ index.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+  <title>Acceuil</title>
+</head>
+<body>
+<?php include 'navbar.php';?>
+
+
+
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,12 @@
+<?php
+echo '<div class="logo_home">
+	   <a href="index.php.html"><img src="img/logo.png" alt="Acceuil" /></a>
+     </div>';
+echo '<div class="navbar">';
+echo "<ul>";
+echo '<li><a class="active" href="#index.php.html">Acceuil</a></li>
+      <li><a href="#">Artiste</a></li>
+      <li><a href="#">Album</a></li>';
+echo "</ul>";
+echo "</div>";
+?>

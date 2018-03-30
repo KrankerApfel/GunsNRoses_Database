20927f051cd1ff95bf4d9b43e37f6f7425c1c214
pillowinacoma <B132011@gmail.com>
2018-03-31 00:21:26 +0200

    modified header animation

--- index.html.php
+++ index.html.php
@@ -12,14 +12,6 @@
 <body>
   <script src="js/script1.js"></script>
   <?php include 'navbar.php';?>
-  <div class="container" >
-    <div class="cadre">
-      <h1>Bienvenue !</h1>
-      <p> La base de donnée Guns and Roses regroupe des informations sur les artistes aillant fait parti du
-          célèbre groupe. Vous y trouverais également la liste des albums. Vous être libre d'ajouter, modifier
-          ou supprimer un poste. <br/>Bonne visite sur notre site !
-      </p>
-<<<<<<< HEAD
 	<div class="container" >
   	<div class="cadre">
     <h1>Bienvenue !</h1>
@@ -31,13 +23,7 @@
       <button class="button" href="#">Effectuer une recherche</button>
       <a href="form_artiste.html.php"><button class="button">Ajouter un artiste</button></a>
       <a href="form_album.html.php"><button class="button">Ajouter un album</button></a>
-=======
 
-        <button class="button" href="#">Effectuer une recherche</button>
-        <a href="form_artiste.html.php"><button class="button">Ajouter un artiste</button></a>
-        <button class="button">Ajouter un album</button>
-    </div>
->>>>>>> 89efe88afbf2548052759268f3ae53fb99c7d182
   </div>
   <?php include 'footer.php' ?>
 </body>

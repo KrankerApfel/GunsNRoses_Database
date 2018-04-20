c5316052b1311cdcd72edb9c8d14908db7cb13b0
Abdelaziz Sbaai <sa175140@localhost.univlehavre.lan>
2018-04-20 14:02:28 +0200

    rien2

--- form_artiste.php
+++ form_artiste.php
@@ -8,7 +8,6 @@ include 'functions.php';
 * TODO Tahina
 */
 
-<<<<<<< HEAD
 $tab = array('nom', 'prenom', 'instrument');
 $row_art = array( 'art_pseudo' => null,
               'art_nom' => null,
@@ -16,9 +15,6 @@ $row_art = array( 'art_pseudo' => null,
               'art_dateNaissance' => null,
               'art_dateMort' => null);
 $row_participe = array();
-=======
-$tab = array('art_nom', 'art_prenom', 'instrument');
->>>>>>> c18eba61ad83fd2c3945721630034596939ed9f7
 
 if (valideForm($_GET, $tab)) {
   echo '<div class="container" >

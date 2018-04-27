4a074e36b3bada2f512fb05763c21db28f9b19b2
Tom LaPomme <tissmey76@gmail.com>
2018-04-27 17:55:53 +0200

    css

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <?php echo '<div class="footer">
-  <p>❤ Réalisé par : Tahina Rakotomanampison | Brice Simon | Abdelaziz Sbaâi ❤</p>
+  <p>❤ Réalisé par : <a href="https://github.com/KrankerApfel"> Tahina Rakotomanampison </a>|  <a href="https://github.com/Decim298">Brice Simon</a> |  <a href="https://github.com/pillowinacoma">Abdelaziz Sbaâi</a> ❤</p>
 </div>';
  ?>
